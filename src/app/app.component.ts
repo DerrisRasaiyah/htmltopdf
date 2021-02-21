da3208640f704841de33ffa94b13c888816af486
DerrisRasaiyah <49778041+DerrisRasaiyah@users.noreply.github.com>
2021-02-21 18:46:04 +0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,37 +10,28 @@ import * as data from './data.json';
 })
 export class AppComponent implements OnInit {
   tncContent: string;
-  userRole: string = 'hcp'
+  userRole: string = 'admin'
   showModal: boolean = false;
   roles: any = (data as any).default.roles;
   doc = new jsPDF({
     orientation: 'p',
     unit: 'pt',
     format: 'a4',
+  })
 
-  });
   constructor() { }
+
   ngOnInit() {
     let currentRoleMatch = this.roles.filter(role => role.name === this.userRole);
     if (currentRoleMatch.length > 0) {
       this.tncContent = currentRoleMatch[0].tnc;
-      console.log(this.tncContent);
     }
   }
 
   showTnC() {
     if (this.tncContent) {
-
-      // this.doc.text("Lorem Ipsum is simply dummy text of the printing and typesetting industry.,\n" + "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum", 100, 100)
-      // this.doc.save("exp.pdf")
-
-      this.doc.html(this.tncContent, {
-        callback: function (doc) {
-          doc.setLineWidth(200)
-          doc.save("demo1.pdf");
-        },
-        margin: 100
-      })
+      this.doc.fromHTML(this.tncContent, 30,30, { 'width': 550});
+      this.doc.save("Terms.pdf");
     }
   }
 
