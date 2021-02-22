066b9d4b441316503d7d644eca31bbe6ebd66daa
DerrisRasaiyah <49778041+DerrisRasaiyah@users.noreply.github.com>
2021-02-22 11:04:22 +0400

    Update modal.component.ts

--- src/app/Presentation/modal.component.ts
+++ src/app/Presentation/modal.component.ts
@@ -4,44 +4,32 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     selector: 'app-modal',
     template: `
     <div class="modal" role="dialog" [ngClass]="{'show': isShow}">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Terms and Condition</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close" (click)="onCloseModel()">
-          <span aria-hidden="true">&times;</span>
-        </button>
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Terms and Condition</h5>
+            <button type="button" class="close" aria-label="Close" (click)="onCloseModal()">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <ng-content></ng-content>
+          </div>
+        </div>
       </div>
-      <div class="modal-body">
-        <p><ng-content></ng-content></p>
-      </div>
-      <!-- <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal" (click)="onCloseModel()">Close</button>
-      </div> -->
     </div>
-  </div>
-</div>
     `,
-    styles: [
-        `
-        .modal { overflow: auto }
-        .modal.show {
-            display: block;
-        }
-        `
-    ]
+    styles: [ `.modal.show { display: block; overflow: auto } `]
 })
 
 export class ModalComponent {
     @Input('isShow') isShow: boolean;
-    @Output() changeShow = new EventEmitter<boolean>();
-    constructor() {
+    @Output() changeShowValue = new EventEmitter<boolean>();
+    constructor() { }
 
-    }
-
-    onCloseModel() {
+    onCloseModal() {
         this.isShow = false;
-        this.changeShow.emit(this.isShow)
+        this.changeShowValue.emit(this.isShow)
     }
 
 }
